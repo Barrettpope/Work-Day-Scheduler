5704d4fec71a651abc98f394121f136926d50ed7
Barrett Pope <barrettpope94@gmail.com>
2020-07-16 11:32:48 -0400

    Styling changes made to CSS and logic added to JS file

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,52 @@
-// Assigning variables to selected HTML elements
+// Creating global variables
 var dateParagraph = $("#currentDay");
-var date = moment();
+var date = moment().format("dddd, MMMM Do YYYY");
+var hour = parseInt(moment().format("HH"));
+var timeSlots = [];
+userInput = [];
 var toDos = $(".form-control");
-var saveButton = $(".btn");
-
-// Creating global variable for user input
-userInput = "";
-
-// Adding an event listener to the save buttons
-saveButton.on("click", saveInput());
+var saveButton = $(".save-button");
+var resetColors = setInterval(changeColor, 360000)
 
 // Displays current date in header
 function displayDate() {
-
+    dateParagraph.text(date);
 }
-
-// Stores user input in text box and saves to local storage
-function saveInput() {
-    localStorage.setItem("", "");
-    toDos.innerHTML = localStorage.getItem("");
-}
+displayDate();
 
 // Modifies color to reflect past, present and future time
 function changeColor() {
+    hour = parseInt(moment().format("HH"));
 
+    for (var i = 8; i <= 17; i++) {
+        timeSlots[i] = $(`#${i}`)
+        if (i < hour) {
+            $(`#slot${i}`).addClass("past");
+        } else if (i === hour) {
+            $(`#slot${i}`).addClass("present");
+        } else {
+            $(`#slot${i}`).addClass("future");
+        }
+    }
 }
+changeColor();
+
+// Saves user input to local storage when save button is clicked
+saveButton.on("click", function (event) {
+    event.preventDefault();
+    var clickButton = $(this);
+    userInput = JSON.parse(localStorage.getItem("todo"));
+    if (!userInput) {
+        userInput = [];
+    }
+    var buttonID = clickButton.attr("id");
+    buttonID = buttonID.replace("button", "");
+    var valueSlot = $(`#slot${buttonID}`).val();
+    var time = $(`#${buttonID}`).text();
+
+    userInput.push({
+        "time": time,
+        "task": valueSlot
+    })
+    localStorage.setItem("todo", JSON.stringify(userInput));
+});
